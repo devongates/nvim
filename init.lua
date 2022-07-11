0c60b3704b4c45c8ade08c226dbcb9c8e20bfc71
Devon Gates <devongates@Devons-Air.attlocal.net>
2022-07-11 09:34:00 -0500

    Added initial keymaps

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 require "usr.options"
+require "usr.keymaps"
--- lua/usr/options.lua
+++ lua/usr/options.lua
@@ -1,56 +1,56 @@
 local options = {
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 500,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  ttimeoutlen = 10,
-  undofile = true,                         -- enable persistent undo
-  updatetime = 1000,                       -- faster completion (4000ms default)
-  tabstop = 4,                             -- insert 2 spaces for a tab
-  shiftwidth = 0,                          -- the number of spaces inserted for each indentation
-  expandtab = true,                        -- convert tabs to spaces
-  softtabstop = -1,
-  shiftround = true,
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = true,                   -- set relative numbered lines
-  signcolumn = "yes:1",                    -- always show the sign column, otherwise it would shift the text each time
-  wrap = false,                            -- display lines as one long line
-  scrolloff = 8,                           -- is one of my fav
-  sidescrolloff = 8,
-  whichwrap = vim.opt.whichwrap:append "<>[]hl",
-  iskeyword = vim.opt.iskeyword:append "-",
-  listchars = { eol = "↲", tab = "▶ ", trail = "•", precedes = "«", extends = "»", nbsp = "␣", space = "." },
-  syntax = "ON",        -- str:  Allow syntax highlighting
-  wildignore = { ".git", ".hg", ".svn", "*.pyc", "*.o", "*.out", "*.jpg", "*.jpeg", "*.png", "*.gif", "*.zip" },
-  wildignore = vim.opt.wildignore + { "**/node_modules/**", "**/bower_modules/**", "__pycache__", "*~", "*.DS_Store" },
-  wildignore = vim.opt.wildignore + { "**/undo/**", "*[Cc]ache/" },
-  wildignorecase = true,
-  infercase = true,
-  lazyredraw = true,
-  showmatch = true,
-  matchtime = 2,
-  synmaxcol = 128, -- avoid slow rendering for long lines
-  pumblend = 15,
-  linebreak = true,
-  wrap = true,
-  mouse = "a",
+    clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    fileencoding = "utf-8",                  -- the encoding written to a file
+    hlsearch = true,                         -- highlight all matches on previous search pattern
+    ignorecase = true,                       -- ignore case in search patterns
+    mouse = "a",                             -- allow the mouse to be used in neovim
+    pumheight = 10,                          -- pop up menu height
+    showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+    smartcase = true,                        -- smart case
+    smartindent = true,                      -- make indenting smarter again
+    splitbelow = true,                       -- force all horizontal splits to go below current window
+    splitright = true,                       -- force all vertical splits to go to the right of current window
+    termguicolors = true,                    -- set term gui colors (most terminals support this)
+    timeoutlen = 500,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+    ttimeoutlen = 10,
+    undofile = true,                         -- enable persistent undo
+    updatetime = 1000,                       -- faster completion (4000ms default)
+    tabstop = 4,                             -- insert 2 spaces for a tab
+    shiftwidth = 0,                          -- the number of spaces inserted for each indentation
+    expandtab = true,                        -- convert tabs to spaces
+    softtabstop = -1,
+    shiftround = true,
+    cursorline = true,                       -- highlight the current line
+    number = true,                           -- set numbered lines
+    relativenumber = true,                   -- set relative numbered lines
+    signcolumn = "yes:1",                    -- always show the sign column, otherwise it would shift the text each time
+    wrap = false,                            -- display lines as one long line
+    scrolloff = 8,                           -- is one of my fav
+    sidescrolloff = 8,
+    whichwrap = vim.opt.whichwrap:append "<>[]hl",
+    iskeyword = vim.opt.iskeyword:append "-",
+    listchars = { eol = "↲", tab = "▶ ", trail = "•", precedes = "«", extends = "»", nbsp = "␣", space = "." },
+    syntax = "ON",        -- str:  Allow syntax highlighting
+    wildignore = { ".git", ".hg", ".svn", "*.pyc", "*.o", "*.out", "*.jpg", "*.jpeg", "*.png", "*.gif", "*.zip" },
+    wildignore = vim.opt.wildignore + { "**/node_modules/**", "**/bower_modules/**", "__pycache__", "*~", "*.DS_Store" },
+    wildignore = vim.opt.wildignore + { "**/undo/**", "*[Cc]ache/" },
+    wildignorecase = true,
+    infercase = true,
+    lazyredraw = true,
+    showmatch = true,
+    matchtime = 2,
+    synmaxcol = 128, -- avoid slow rendering for long lines
+    pumblend = 15,
+    linebreak = true,
+    wrap = true,
+    mouse = "a",
 }
 
 vim.opt.shortmess:append "c"
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 vim.opt.formatoptions = "l"
