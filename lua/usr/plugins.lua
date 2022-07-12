9dc94dedb46671b47483934c0ab05660c12cabce
Devon Gates <devongates@Devons-Air.attlocal.net>
2022-07-12 10:32:09 -0500

    Added copilot

--- lua/usr/plugins.lua
+++ lua/usr/plugins.lua
@@ -85,6 +85,9 @@ return packer.startup(function(use)
     -- hop-nvim
     use "phaazon/hop.nvim"
 
+    -- copilot
+    use "github/copilot.vim"
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
