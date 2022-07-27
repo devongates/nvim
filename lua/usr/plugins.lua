f21dce38666e080d234e34ac8065570ca95fb0f3
Devon Gates <devongates@Devons-Air.attlocal.net>
2022-07-27 10:34:05 -0500

    Added Comment.nvim

--- lua/usr/plugins.lua
+++ lua/usr/plugins.lua
@@ -104,6 +104,9 @@ return packer.startup(function(use)
         after = { "copilot.lua", "nvim-cmp" },
     }
 
+    -- comment
+    use "numToStr/Comment.nvim"
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
