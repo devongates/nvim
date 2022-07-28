d6ede319ae4c0a1d99c0b03d677cb340dbeaac1c
Devon Gates <devongates@Devons-Air.attlocal.net>
2022-07-28 16:44:13 -0500

    Added alpha-nvim

--- lua/usr/plugins.lua
+++ lua/usr/plugins.lua
@@ -107,6 +107,9 @@ return packer.startup(function(use)
     -- comment
     use "numToStr/Comment.nvim"
 
+    -- title screen
+    use 'goolord/alpha-nvim'
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
